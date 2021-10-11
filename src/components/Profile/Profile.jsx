a36e3c742a3dae2c1cd8c7625608a9f7af44cc79
Dizagler <sid_alex@yahoo.com>
2021-10-11 15:24:38 +0300

    Create Profile.jsx

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import s from './Profile.module.css';
+import Posts from './Posts/Posts';
+
+const Profile = () => {
+  return (
+  <div className={s.content}>
+    <div>
+    <img src="https://media.publit.io/file/Snow-background-night.png" alt="" />
+     </div>
+       <div>
+     ava+description
+   </div>
+   <Posts/>
+   </div>
+  ) 
+}
+
+export default Profile;
