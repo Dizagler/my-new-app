30b934c0a3602adfe1509ae8e5d7282ca5dc1dcc
Dizagler <sid_alex@yahoo.com>
2021-10-12 09:42:22 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import { BrowserRouter, Route} from "react-router-dom";
+import Music from './components/Music/Music';
+import News from './components/News/News';
+import Settings from './components/Settings/Settings';
 
 function App() {
   return (
@@ -15,6 +18,9 @@ function App() {
      <div class='app-wrapper-content'>
        <Route path='/dialogs' component = {Dialogs} />
        <Route path='/profile' component = {Profile} />
+       <Route path='/music' component = {Music} />
+       <Route path='/news' component = {News} />
+       <Route path='/settings' component = {Settings} />
      </div>
     </div>
     </BrowserRouter>)
