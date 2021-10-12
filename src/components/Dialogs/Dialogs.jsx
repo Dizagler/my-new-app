cd3a2e27b0eb1026222547f841d10a5c74d1f30a
Dizagler <sid_alex@yahoo.com>
2021-10-12 10:25:29 +0300

    Update Dialogs.jsx

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css';
 
 
 const Dialogs = (props) => {
-   return (
-     <div>Dialogs</div>
-   )
+  return (
+    <div className={s.dialogs}>
+    <div className={s.dialogsItems}>
+      <div className={s.dialog + ' ' + s.active}>
+        <NavLink to='/dialogs/1'>Dima</NavLink>
+      </div>
+      <div className={s.dialog}>
+          <NavLink to='/dialogs/2'>Andrey</NavLink>
+      </div>
+      <div className={s.dialog}>
+          <NavLink to='/dialogs/3'>Ira</NavLink>
+      </div>
+      <div className={s.dialog}>
+          <NavLink to='/dialogs/4'>Sveta</NavLink>
+      </div>
+      <div className={s.dialog}>
+          <NavLink to='/dialogs/5'>Uliya</NavLink>
+      </div>
+      <div className={s.dialog}>
+          <NavLink to='/dialogs/6'>Ivan</NavLink>
+      </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>How are yoy?</div>
+        <div className={s.message}>Yo</div>
+      </div>
+    </div>
+    
+  )
 }
 
 export default Dialogs;
