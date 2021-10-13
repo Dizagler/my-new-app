5c0f4fc91690503b2a4f349725e01c597d18d362
Dizagler <sid_alex@yahoo.com>
2021-10-13 15:14:53 +0300

    Update FriendsBlock.jsx

--- src/components/Navbar/FriendsBlock.jsx
+++ src/components/Navbar/FriendsBlock.jsx
@@ -4,6 +4,7 @@ import s from './Navbar.module.css';
 const FriendsBlock = (props) => {
    return (
     <div className={s.friendsBlock}>
+       <img src="https://cdn-icons-png.flaticon.com/512/147/147144.png" alt="" />
       {props.friends}
      </div>
   )
