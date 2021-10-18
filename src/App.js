dd8fbed2a4eaa95ad583b790c30786d016e45c97
Dizagler <sid_alex@yahoo.com>
2021-10-18 08:12:36 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = (props) => {
      <Header />
      <Navbar state={props.state.friendsNav} />
      <div class='app-wrapper-content'>
-        <Route path='/dialogs' render = { () => <Dialogs dialogsPage={props.state.dialogsPage} addMessageDialog={props.addMessageDialog} updateNewMessage={props.updateNewMessage}  /> } />
+       <Route path='/dialogs' render = { () => <Dialogs dialogsPage={props.state.dialogsPage} addMessageDialog={props.addMessageDialog} updateNewMessage={props.updateNewMessage}  /> } />
        <Route path='/profile' render = { () => <Profile profilePage={props.state.profilePage} addPost={props.addPost} updateNewPostText={props.updateNewPostText} /> } />
        <Route path='/music' render = { () => <Music /> } />
        <Route path='/news' render = { () => <News /> } />
